42164a695d3e05dbdf730bb9f3af0e412df1a274
Nomie69K <157805964+Normie69K@users.noreply.github.com>
2025-09-14 11:34:34 +0530

    Replace signup and login with getstarted button

--- app/src/main/java/com/sih/apkaris/GetStartedActivity.kt
+++ app/src/main/java/com/sih/apkaris/GetStartedActivity.kt
@@ -3,8 +3,8 @@ package com.sih.apkaris
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 
 class GetStartedActivity : AppCompatActivity() {
 
@@ -23,24 +23,26 @@ class GetStartedActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_get_started)
 
+
+
         val btnSignUp: Button = findViewById(R.id.textView7)
-        val btnRegister: TextView = findViewById(R.id.textView4)
+//        val btnRegister: TextView = findViewById(R.id.textView4)
 
         btnSignUp.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java).apply {
                 putExtra("showFragment", "login")
             })
-            prefs.edit().putBoolean("isFirstRun", false).apply()
+            prefs.edit { putBoolean("isFirstRun", false) }
             finish()
         }
 
-        btnRegister.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java).apply {
-                putExtra("showFragment", "register")
-            })
-            prefs.edit().putBoolean("isFirstRun", false).apply()
-            finish()
-        }
+//        btnRegister.setOnClickListener {
+//            startActivity(Intent(this, MainActivity::class.java).apply {
+//                putExtra("showFragment", "register")
+//            })
+//            prefs.edit().putBoolean("isFirstRun", false).apply()
+//            finish()
+//        }
     }
 }
 
