24283979d8d7c611b38e1ed70e725a1f4536507f
Nomie69K <157805964+Normie69K@users.noreply.github.com>
2025-09-23 16:13:36 +0530

    Modify

--- app/src/main/java/com/sih/apkaris/fragements/HomeFragment.kt
+++ app/src/main/java/com/sih/apkaris/fragements/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.sih.apkaris.fragments
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.sih.apkaris.MainActivity
+import com.sih.apkaris.R
 import com.sih.apkaris.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,6 +25,27 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mainActivity = activity as? MainActivity
 
+        updateThemeIcon()
+
+        binding.buttonThemeSwitcher.setOnClickListener {
+            toggleTheme()
+        }
+
+
+//        binding.fabThemeSwitcher.setOnClickListener {
+//            // Check what the current night mode is
+//            val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+//
+//            if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+//                // If it's currently night, switch to day mode
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//            } else {
+//                // If it's currently day, switch to night mode
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//            }
+//        }
+
+
         binding.buttonSettings.setOnClickListener {
             mainActivity?.showProfileFragment()
         }
@@ -36,6 +60,30 @@ class HomeFragment : Fragment() {
         }
         binding.buttonReportFound.setOnClickListener {
             mainActivity?.showReportFoundFragment()
+        }
+    }
+
+    private fun toggleTheme() {
+        // Check what the current night mode is
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            // If it's currently night, switch to day mode
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        } else {
+            // If it's currently day, switch to night mode
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+    }
+
+    private fun updateThemeIcon() {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            // It's dark mode, show the sun icon to switch to light
+            binding.buttonThemeSwitcher.setImageResource(R.drawable.ic_sun)
+        } else {
+            // It's light mode, show the moon icon to switch to dark
+            binding.buttonThemeSwitcher.setImageResource(R.drawable.ic_moon)
         }
     }
 
